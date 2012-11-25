f453ec52ad9effd0594cd595174349a7042f7bf8
Mahmud J. Khaled <mahmudkhaled@gmail.com>
2012-11-25 22:03:36 +0000

    updated the index.html with reference to JQuery, new JS libs for inheritence and Application start-up

---
+++ src/Scripts/Application.js
@@ -0,0 +1,26 @@
+﻿//Application.JS
+
+var ScmApp = ScmApp || {};
+ScmApp.Mapping = {};
+//ScmApp.Master = Class.extend({
+//    init: function (pValue) {
+//        this.p = pValue;
+//    },
+//    m: function () {
+//        alert("alert...");
+//    }
+//});
+
+//ScmApp.Slave = ScmApp.Master.extend({
+//    init: function (pValue) {
+//        this._super(pValue);
+//    }
+//});
+
+//
+
+ScmApp.Mapping.MapBuilder = Class.extend({
+    init: function () {
+        //ctor
+    }
+});
